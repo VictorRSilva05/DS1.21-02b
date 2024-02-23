bac1c95c083302cec23c81fd31f98df556d9093f
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-23 09:30:16 -0300

    Finished

---
+++ ConsoleApp21/Entities/Decimals.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21.Entities
+{
+    internal class Decimals
+    {
+        //Victor Rafael da Silva
+        public List<decimal> decimals {  get; set; }
+
+        public Decimals(List<decimal> decimals)
+        {
+            this.decimals = decimals;
+        }
+
+        public decimal AverageValue(List<decimal> decimals)
+        {
+            return decimals.Average();
+        }
+
+        public void EvenOrOdd(List<decimal> decimals)
+        {
+            int integer;
+            foreach (decimal c in decimals)
+            {
+                integer = (int)c;
+                if (integer % 2 == 0)
+                {
+                    Console.WriteLine($"{integer} is even");
+                }
+                else
+                {
+                    Console.WriteLine($"{integer} is odd");
+                }
+            }
+        }
+
+        public void CorrespondingChar(List<decimal> decimals)
+        {
+            foreach (decimal c in decimals)
+            {
+                string[] strings = c.ToString().Split(",");
+                int numbers = int.Parse(strings[1]);
+                char chars = Convert.ToChar(numbers);
+                Console.WriteLine(chars);
+            }
+        }
+    }
+}
---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,38 @@
+﻿using ConsoleApp21.Entities;
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Victor Rafael da Silva
+
+            List<Decimal> list = new List<Decimal>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                decimal result = default(decimal);
+
+                Console.Write($"Insert value number {i + 1}: ");
+                string input = Console.ReadLine();
+
+                while (!decimal.TryParse(input, out result) || result == Math.Floor(result))
+                {
+                    Console.Write("Invalid input, try again: ");
+                    input = Console.ReadLine();
+                }
+                list.Add(result);
+            }
+
+            Decimals decimals = new Decimals(list);
+
+            Console.WriteLine("\nThe average value: " + decimals.AverageValue(list).ToString("F2"));
+
+            Console.WriteLine();
+            decimals.EvenOrOdd(list);
+
+            Console.WriteLine();
+            decimals.CorrespondingChar(list);
+        }
+    }
+}
